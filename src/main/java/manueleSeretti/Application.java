b7c5db096c28a0da01fe38d57d096060b80cd4a3
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-10-13 16:51:05 +0200

    aggiunta menu

--- src/main/java/manueleSeretti/Application.java
+++ src/main/java/manueleSeretti/Application.java
@@ -31,34 +31,151 @@ public class Application {
         for (int i = 0; i < 3; i++) {
             listaRiviste.add(rivistaSupplier.get());
         }
-
-        //listaLibri.add(aggiungiLibro());
-        // listaRiviste.add(aggiungiRivista());
         listaLibri.forEach(System.out::println);
         listaRiviste.forEach(System.out::println);
 
+        //listaLibri.add(aggiungiLibro());
 
-        //EliminaElementoIsbn(listaRiviste);
+        loop:
+        while (true) {
+            try {
+                System.out.println("SCEGLI L'OPRAZIONE DA FATE O DIGITA 0 PER CHIUDERE IL PROGRAMMA");
+                System.out.println("1-Aggiungi un elemento:");
+                System.out.println("2-Rimuovi un elemento:");
+                System.out.println("3-Ricerca un elemento:");
+                System.out.println("4-Consulta l'archivio:");
+                int scelta = Integer.parseInt(input.nextLine());
+                ciclo:
+                switch (scelta) {
+                    case 0: {
+                        break loop;
+                    }
+                    case 1: {
+                        System.out.println("cosa vuoi aggiungere?");
+                        System.out.println("1-Libro");
+                        System.out.println("2-Rivista");
+                        int s = Integer.parseInt(input.nextLine());
+                        scelta0:
+                        switch (s) {
+                            case 1: {
+                                listaLibri.add(aggiungiLibro());
+                                break scelta0;
+                            }
+                            case 2: {
+                                listaRiviste.add(aggiungiRivista());
+                                break scelta0;
+                            }
+                            default:
+                                break scelta0;
+                        }
+                        break ciclo;
+                    }
+                    case 2: {
+                        System.out.println("cosa vuoi eliminare?");
+                        System.out.println("1-Libro");
+                        System.out.println("2-Rivista");
+                        int s = Integer.parseInt(input.nextLine());
+                        scelta2:
+                        switch (s) {
+                            case 1: {
+                                EliminaElementoIsbn(listaLibri);
+                                break scelta2;
+                            }
+                            case 2: {
+                                EliminaElementoIsbn(listaRiviste);
+                                break scelta2;
 
-        //ricercaIsbn(listaRiviste);
+                            }
+                            default:
+                                break scelta2;
 
-        //ricercaAnno(listaRiviste);
+                        }
+                        break ciclo;
+                    }
+                    case 3: {
+                        System.out.println("fai la ricerca in base:");
+                        System.out.println("1- per codice isbn:");
+                        System.out.println("2- per anno di publicazione:");
+                        System.out.println("3- per Autore");
+                        int s = Integer.parseInt(input.nextLine());
+                        scelta3:
+                        switch (s) {
+                            case 1: {
+                                System.out.println("cosa vuoi cercare?");
+                                System.out.println("1-Libro");
+                                System.out.println("2-Rivista");
+                                int c = Integer.parseInt(input.nextLine());
+                                switch (c) {
+                                    case 1: {
+                                        ricercaIsbn(listaLibri);
+                                        break scelta3;
+                                    }
+                                    case 2: {
+                                        ricercaIsbn(listaRiviste);
+                                        break scelta3;
+                                    }
+                                    default:
+                                        break scelta3;
+                                }
+                            }
+                            case 2: {
+                                System.out.println("cosa vuoi cercare?");
+                                System.out.println("1-Libro");
+                                System.out.println("2-Rivista");
+                                int x = Integer.parseInt(input.nextLine());
+                                switch (x) {
+                                    case 1: {
+                                        ricercaAnno(listaLibri);
+                                        break scelta3;
+                                    }
+                                    case 2: {
+                                        ricercaAnno(listaRiviste);
+                                        break scelta3;
+                                    }
+                                    default:
+                                        break scelta3;
+                                }
 
-        // ricercaAutore(listaLibri);
+                            }
+                            case 3: {
+                                ricercaAutore(listaLibri);
+                                break scelta3;
+                            }
+                            default:
+                                break scelta3;
+                        }
+                        break ciclo;
+                    }
+                    case 4: {
+                        try {
+                            saveToDisk(listaLibri, listaRiviste);
+                        } catch (IOException ex) {
+                            System.err.println(ex.getMessage());
+                        }
 
-        try {
-            saveToDisk(listaLibri, listaRiviste);
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
+                        try {
+                            catalogo = loadFromDisk();
+                        } catch (IOException ex) {
+                            System.err.println(ex.getMessage());
+                        }
+
+                        catalogo.forEach(System.out::println);
+                        break ciclo;
+                    }
+                    default: {
+                        System.out.println("codice inserito non valido!!!");
+                        break ciclo;
+                    }
+                }
+
+            } catch (Exception x) {
+                System.err.println(x);
+            }
+
+
         }
 
-        try {
-            catalogo = loadFromDisk();
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-        }
 
-        catalogo.forEach(System.out::println);
     }
 
     public static Libro aggiungiLibro() {
@@ -69,25 +186,21 @@ public class Application {
         int anno;
         int pag;
         String title;
-        while (true) {
-            try {
-                System.out.println("inserisci titolo");
-                title = input.nextLine();
-                System.out.println("inserisci anno di pubblicazione");
-                anno = Integer.parseInt(input.nextLine());
-                System.out.println("inserisci numero di pagine");
-                pag = Integer.parseInt(input.nextLine());
-                System.out.println("inserisci autore");
-                autore = input.nextLine();
-                System.out.println("inserisci genere");
-                genere = input.nextLine();
-                input.close();
-                break;
-            } catch (Exception ex) {
-                System.out.println("Il valore inserito non è accettabile!!!");
-                System.err.println(ex.getMessage());
-            }
-        }
+
+
+        System.out.println("inserisci titolo");
+        title = input.nextLine();
+        System.out.println("inserisci anno di pubblicazione");
+        anno = Integer.parseInt(input.nextLine());
+        System.out.println("inserisci numero di pagine");
+        pag = Integer.parseInt(input.nextLine());
+        System.out.println("inserisci autore");
+        autore = input.nextLine();
+        System.out.println("inserisci genere");
+        genere = input.nextLine();
+        input.close();
+
+
         return new Libro(title, anno, pag, autore, genere);
     }
 
